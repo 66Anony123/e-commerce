4cc541e336124ab945792d0f32220358c67d7bb2
66Anony123 <478gnujnoej@gmail.com>
2023-06-04 16:42:41 +0545

    first commit e-comm

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+
+
+<header>
+    <ul>
+        <li><a href ="/home">Home</a></li>
+        <li><a href ="/about">About</a></li>
+        <li><a href ="/login">COntact</a></li>
+    </ul>
+</header>
+<main>
+    <p>
+   THis is about page
+    </p>
+</main>
+
+<footer>
+    <h1>THamk you for visitin</h1>
+</footer>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('content')
+
+<h1> Login</h1>
+
+<form>
+
+<input type= 'text' name="" id="" placeholder="username"/>
+<input type= 'password' name="" id="" placeholder="password"/>
+
+<button> Login</button>
+</form>
+
+@endsection
